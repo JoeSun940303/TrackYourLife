8ea4f4ec2f3f4dc564ea1e5787a9a26730bb0a03
JoeSun <joesun@JoeSuns-MacBook-Pro.local>
2017-07-19 14:06:06 -0500

    add a easy nodejs app to handle the form submit, but a notice here is that the way I used is to use the ip address, is not a good way, I will change it later

--- LocalServer.js
+++ LocalServer.js
@@ -1,8 +1,17 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("Your request is received");
-    res.end();
-}).listen(8080);
+var express = require('express');
+var bodyParser = require('body-parser');
+var app     = express();
+
+//Note that in version 4 of express, express.bodyParser() was
+//deprecated in favor of a separate 'body-parser' module.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//app.use(express.bodyParser());
+
+app.post('/myaction', function(req, res) {
+  res.send("We have recieved your information, your registered account name is "+req.body.accountname+".");
+});
+
+app.listen(8080, function() {
+  console.log('Server running ');
+});
